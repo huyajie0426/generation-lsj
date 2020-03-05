f81b08b255d029fa3fdafb0ec78e945da064e811
yang <Agustinymy@outlook.com>
2020-03-05 20:41:33 +0800

    页面URL修改

--- src/main/java/com/lsj/controller/TbUserController.java
+++ src/main/java/com/lsj/controller/TbUserController.java
@@ -12,4 +12,73 @@ public class TbUserController {
 		return "index";
 	}
 	
+	@RequestMapping(value="/integral")
+	public String goIntegral() {
+		return "integral";
+	}
+	
+	@RequestMapping(value="/login")
+	public String goLogin() {
+		return "login";
+	}
+	@RequestMapping(value="/order")
+	public String goOrder() {
+		return "order";
+	}
+	
+	@RequestMapping(value="/productDetailes")
+	public String goProductDetailes() {
+		return "productDetailes";
+	}
+	
+	@RequestMapping(value="/productList")
+	public String goProductList() {
+		return "productList";
+	}
+	
+	@RequestMapping(value="/products")
+	public String goProducts() {
+		return "products";
+	}
+	
+	@RequestMapping(value="/register")
+	public String goRegister() {
+		return "register";
+	}
+	
+	@RequestMapping(value="/shoppingCart")
+	public String goShoppingCart() {
+		return "shoppingCart";
+	}
+	
+	@RequestMapping(value="/user")
+	public String goUser() {
+		return "user";
+	}
+	
+	@RequestMapping(value="/userAddress")
+	public String goUserAddress() {
+		return "userAddress";
+	}
+	
+	@RequestMapping(value="/userCollect")
+	public String goUserCollect() {
+		return "userCollect";
+	}
+	
+	@RequestMapping(value="/userInfo")
+	public String goUserInfo() {
+		return "userInfo";
+	}
+	
+	@RequestMapping(value="/userIntegral")
+	public String goUserIntegral() {
+		return "userIntegral";
+	}
+	
+	@RequestMapping(value="/userPassword")
+	public String goUserPassword() {
+		return "userPassword";
+	}
+	
 }
